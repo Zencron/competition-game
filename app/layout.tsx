5869d1d08661eb110c37554165028799c5f31062
Justin Chong <justinchongzhiyu@gmail.com>
2025-12-10 17:52:03 +0800

    feat: initial layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="flex min-h-dvh flex-col p-2">
+          <div className="flex grow items-center justify-center p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-xs lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
