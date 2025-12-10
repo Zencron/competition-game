fa728802c141fbad05600a2e81642e2e137a97ad
Justin Chong <justinchongzhiyu@gmail.com>
2025-12-10 17:57:41 +0800

    refactor: style darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
+      <body className={`${inter.variable} font-sans antialiased`}>
         <main className="flex min-h-dvh flex-col p-2">
           <div className="flex grow items-center justify-center p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-xs lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
             {children}
